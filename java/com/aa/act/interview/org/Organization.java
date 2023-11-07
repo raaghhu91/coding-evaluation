4e0d39bd417c544fa4d1c7799816db8072cc84ac
RAAGHHU <raaghhu@RAAGHHUs-MBP.lan>
2023-11-07 15:25:42 -0600

    Implemented hire method

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -12,13 +12,36 @@ public abstract class Organization {
     
     protected abstract Position createOrganization();
     
-    /**
-     * hire the given person as an employee in the position that has that title
-     * 
-     * @param person
-     * @param title
-     * @return the newly filled position or empty if no position has that title
-     */
+    public Optional<Position> hire(Name person, String title) {
+    return hireHelper(root, person, title);
+}
+
+private Optional<Position> hireHelper(Position position, Name person, String title) {
+    // Check if the current position's title matches the title we are hiring for
+    if (position.getTitle().equals(title) && !position.isFilled()) {
+        // Create a new Employee and set their name and identifier
+        Employee employee = new Employee(generateEmployeeIdentifier(), person);
+        position.setEmployee(Optional.of(employee));
+        return Optional.of(position);
+    } else {
+        // Recursively search for the correct position in the direct reports
+        for (Position directReport : position.getDirectReports()) {
+            Optional<Position> hiredPosition = hireHelper(directReport, person, title);
+            if (hiredPosition.isPresent()) {
+                return hiredPosition;
+            }
+        }
+    }
+    return Optional.empty();
+}
+
+// A helper method to generate a unique identifier for each Employee
+private int employeeIdentifier = 1;
+
+private int generateEmployeeIdentifier() {
+    return employeeIdentifier++;
+}
+
     public Optional<Position> hire(Name person, String title) {
         //your code here
         return Optional.empty();
